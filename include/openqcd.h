02420dfaee7f9a0ad3ffd32926f97a55cff62822
Jonas Glesaaen <jonas@glesaaen.com>
2018-04-27 21:02:42 +0100

    Added an all-inclusive header file

---
+++ include/openqcd.h
@@ -0,0 +1,48 @@
+
+/*******************************************************************************
+ *
+ * File openqcd.h
+ *
+ * Copyright (C) 2018 Jonas Rylund Glesaaen
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License (GPL)
+ *
+ * Collection of all openQCD headers
+ *
+ *******************************************************************************/
+
+#ifndef OPENQCD_H
+#define OPENQCD_H
+
+#include "archive.h"
+#include "block.h"
+#include "dfl.h"
+#include "dirac.h"
+#include "field_com.h"
+#include "flags.h"
+#include "forces.h"
+#include "global.h"
+#include "lattice.h"
+#include "linalg.h"
+#include "linsolv.h"
+#include "little.h"
+#include "mdflds.h"
+#include "openqcd.h"
+#include "random.h"
+#include "ratfcts.h"
+#include "sap.h"
+#include "sflds.h"
+#include "stout_smearing.h"
+#include "su3fcts.h"
+#include "su3.h"
+#include "sw_term.h"
+#include "tcharge.h"
+#include "uflds.h"
+#include "update.h"
+#include "utils.h"
+#include "version.h"
+#include "vflds.h"
+#include "wflow.h"
+
+#endif /* OPENQCD_H */
