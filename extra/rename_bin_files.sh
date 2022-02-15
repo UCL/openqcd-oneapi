e55b42b2763ca86540c99c12cb5ef7f3b7e3224c
Tuomas Koskela <t.koskela@ucl.ac.uk>
2022-02-15 17:02:24 +0000

    Conform to Makis naming convention

--- extra/rename_bin_files.sh
+++ extra/rename_bin_files.sh
@@ -2,8 +2,14 @@
 
 mkdir -p processed &> /dev/null
 EXTENSION="bin"
-for fn in $(find . -maxdepth 1 -name "*.$EXTENSION")
+for fn in $(find . -maxdepth 1 -name "pi*.$EXTENSION")
 do
     BASENAME=$(basename --suffix=".$EXTENSION" $fn)
-    mv $fn processed/$BASENAME-$1-$2-$3-$4.$EXTENSION
+    mv $fn processed/$BASENAME-$1-$2-$3-$4
+done
+
+for fn in $(find . -maxdepth 1 -name "[a-z].$EXTENSION")
+do
+    BASENAME=$(basename --suffix=".$EXTENSION" $fn)
+    mv $fn processed/sp-$BASENAME-$1-$2-$3-$4
 done
