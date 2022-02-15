f7e2868647e261c0505dc1c813369965f292fd37
Tuomas Koskela <t.koskela@ucl.ac.uk>
2022-02-15 11:15:00 +0000

    Add quotes

--- extra/rename_bin_files.sh
+++ extra/rename_bin_files.sh
@@ -2,7 +2,7 @@
 
 mkdir -p processed &> /dev/null
 EXTENSION="bin"
-for fn in $(find . -maxdepth 1 -name *.$EXTENSION)
+for fn in $(find . -maxdepth 1 -name "*.$EXTENSION")
 do
     BASENAME=$(basename --suffix=".$EXTENSION" $fn)
     mv $fn processed/$BASENAME-$1-$2-$3-$4.$EXTENSION
